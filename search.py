f334282e1613426ce5210dd0cc54c570bb1dead5
Thiago P. Bueno <thiago.pbueno@gmail.com>
2016-08-15 12:07:24 -0300

    Implement breadthFirstSearch function

--- search.py
+++ search.py
@@ -124,8 +124,35 @@ def depthFirstSearch(problem):
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # "*** YOUR CODE HERE ***"
+    # util.raiseNotDefined()
+
+    fringe = util.Queue()
+    visited = set()
+
+    initialState = problem.getStartState()
+    initialNode = SearchNode(initialState, None, None, 0, 0)
+    fringe.push(initialNode)
+
+    while not fringe.isEmpty():
+        node = fringe.pop()
+        state = node.state
+
+        if problem.isGoalState(state):
+            solution = []
+            while not node.parent is None:
+                solution.append(node.action)
+                node = node.parent
+            return solution[::-1]
+
+        visited.add(state)
+        for nextState, action, cost in problem.getSuccessors(state):
+            if not nextState in visited:
+                nextNode = SearchNode(nextState, node, action, node.cost + cost, node.height + 1)
+                fringe.push(nextNode)
+
+    print "Solution not found"
+    sys.exit(1)
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
